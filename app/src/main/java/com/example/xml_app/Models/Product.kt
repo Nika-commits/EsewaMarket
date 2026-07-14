09d9b1e9e49fe7ca9f30d03d474f92fc96700758
Your Name <itismepranish@gmail.com>
2026-07-14 16:40:00 +0545

    chore: Created Products Data Class

--- app/src/main/java/com/example/xml_app/Models/Product.kt
+++ app/src/main/java/com/example/xml_app/Models/Product.kt
@@ -1,3 +1,10 @@
 package com.example.xml_app.Models
 
-data class Product(val id: String, val name: String, val imageUrl: Int, val price: Int)
+data class Product (
+    val id: String,
+    val name: String,
+    val imageUrl: Int,
+    val price: Int,
+    val status: String,
+    val brand: String
+)
