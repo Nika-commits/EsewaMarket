5216032d3959fe44430ec49e7d37698fa6e0d404
Your Name <itismepranish@gmail.com>
2026-07-17 14:29:06 +0545

    chore: added Product Repository

--- app/src/main/java/com/example/xml_app/Repository/ProductRepository.kt
+++ app/src/main/java/com/example/xml_app/Repository/ProductRepository.kt
@@ -1,4 +1,7 @@
 package com.example.xml_app.Repository
 
+import com.example.xml_app.Api.RetrofitInstance
+
 class ProductRepository {
+    suspend fun getFeaturedProducts() = RetrofitInstance.api.getFeaturedProducts()
 }
--- app/src/main/java/com/example/xml_app/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/xml_app/ViewModel/HomeViewModel.kt
@@ -1,20 +1,25 @@
 package com.example.xml_app.ViewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.xml_app.Api.RetrofitInstance
 import com.example.xml_app.Models.Product
+import com.example.xml_app.Repository.ProductRepository
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
-    val featuredProducts = MutableLiveData<List<Product>>()
+
+    private val repository = ProductRepository()
+    private val _featuredProducts = MutableLiveData<List<Product>>()
+
+    val featuredProducts: LiveData<List<Product>> = _featuredProducts
 
     fun getFeaturedProduct() {
         viewModelScope.launch {
-            val response = RetrofitInstance.api.getFeaturedProducts()
+            val response = repository.getFeaturedProducts()
             if (response.isSuccessful) {
-                featuredProducts.value = response.body()
+                _featuredProducts.value = response.body()
             }
         }
     }
