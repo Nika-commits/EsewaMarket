9ef21bc04f19e0b8224c558f3572f70189fe9582
Your Name <itismepranish@gmail.com>
2026-07-29 16:06:33 +0545

    chore: setup toolbar in more

--- app/src/main/java/com/example/xml_app/fragments/More.kt
+++ app/src/main/java/com/example/xml_app/fragments/More.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.xml_app.R
 import com.example.xml_app.databinding.FragmentMoreBinding
 
 private const val ARG_PARAM1 = "param1"
@@ -23,5 +26,30 @@ class More : Fragment() {
     ): View {
         _binding = FragmentMoreBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
+    }
+
+    fun setupToolbar() {
+        val toolbar = binding.moreToolbar.toolbar
+        toolbar.title = "More"
+
+        (requireContext() as AppCompatActivity).apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+            }
+        }
+
+        toolbar.navigationIcon?.setTint(
+            ContextCompat.getColor(requireContext(), R.color.textDark)
+        )
+
+        toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
