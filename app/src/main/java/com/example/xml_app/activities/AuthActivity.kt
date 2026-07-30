30b9fd457785fb7687c82e973f0dd7f009d2f8b5
Your Name <itismepranish@gmail.com>
2026-07-30 15:00:04 +0545

    chore: setup sign up page

--- app/src/main/java/com/example/xml_app/activities/AuthActivity.kt
+++ app/src/main/java/com/example/xml_app/activities/AuthActivity.kt
@@ -1,6 +1,10 @@
 package com.example.xml_app.activities
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -50,4 +54,19 @@ class AuthActivity : AppCompatActivity() {
             .commit()
     }
 
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
 }
