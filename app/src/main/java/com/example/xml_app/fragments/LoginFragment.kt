de0ef3d2c9596548cae36544e82983a3b1de1a92
Your Name <itismepranish@gmail.com>
2026-07-30 11:00:16 +0545

    chore: added register xml

--- app/src/main/java/com/example/xml_app/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/xml_app/fragments/LoginFragment.kt
@@ -21,10 +21,10 @@ class LoginFragment : Fragment() {
 
     private val viewModel: LoginViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
