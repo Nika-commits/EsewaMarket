97543eb268f6832ccbbef89b5bc674006f78c282
Your Name <itismepranish@gmail.com>
2026-08-05 11:52:32 +0545

    chore: added User and Cart Dao

--- app/src/main/java/com/example/xml_app/data/dao/CartDao.kt
+++ app/src/main/java/com/example/xml_app/data/dao/CartDao.kt
@@ -1,11 +1,35 @@
 package com.example.xml_app.data.dao
 
-import androidx.room3.Upsert
+import androidx.room3.Insert
+import androidx.room3.Query
+import androidx.room3.Update
 import com.example.xml_app.entities.Cart
+import com.example.xml_app.entities.CartItem
+import kotlinx.coroutines.flow.Flow
 
 interface CartDao {
 
-    @Upsert
+    @Insert
     suspend fun upsert(cart: Cart)
 
+    @Query("""select * from carts where user_id = :userId LIMIT 1""")
+    suspend fun getCartByUserId(userId: Int): Cart
+
+    @Query("""select * from cart_items where cart_id = :cartId""")
+    fun observeCartItems(cartId: Int, productId: Int): Flow<List<CartItem>>
+
+    @Query("""select * from cart where cart_id = :cartId and product_id = :productId LIMIT 1 """)
+    suspend fun getCartItem(cartId: Int, productId: Int): CartItem
+
+    @Insert
+    suspend fun insertCartItem(item: CartItem)
+
+    @Update
+    suspend fun updateCartItem(item: CartItem)
+
+    @Query("""delete * from cart_items where cart_id = :cartId and product_id = :productId""")
+    suspend fun deleteCartItem(cartId: Int, productId: Int)
+
+    @Query("""delete * from cart_items where cart_id = :cartId""")
+    suspend fun clearCart(cartId: Int)
 }
--- app/src/main/java/com/example/xml_app/data/dao/UserDao.kt
+++ app/src/main/java/com/example/xml_app/data/dao/UserDao.kt
@@ -4,6 +4,7 @@ import androidx.room3.Dao
 import androidx.room3.Query
 import androidx.room3.Upsert
 import com.example.xml_app.entities.User
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
@@ -14,4 +15,6 @@ interface UserDao {
     @Query("""select * from users where firebase_uid = :firebaseUid LIMIT 1""")
     suspend fun getFirebaseUserById(firebaseUid: String): User?
 
+    @Query("""select * from users where firebase_uid = :firebaseUid LIMIT 1""")
+    fun observeByFirebaseUid(firebaseUid: String): Flow<User>
 }
