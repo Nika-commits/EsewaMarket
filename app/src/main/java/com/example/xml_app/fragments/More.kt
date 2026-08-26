f22af7f904948809bdc43c534da2104a860dd72b
Your Name <itismepranish@gmail.com>
2026-08-26 16:39:06 +0545

    chore: Added Order Activity Navigation in More

--- app/src/main/java/com/example/xml_app/fragments/More.kt
+++ app/src/main/java/com/example/xml_app/fragments/More.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.example.xml_app.R
 import com.example.xml_app.activities.AuthActivity
 import com.example.xml_app.activities.MainActivity
+import com.example.xml_app.activities.OrderActivity
 import com.example.xml_app.databinding.FragmentMoreBinding
 import com.example.xml_app.utils.CustomApplicationContext
 import com.example.xml_app.utils.firebase.AuthRepository
@@ -101,6 +102,13 @@ class More : Fragment() {
         val myOrder = binding.authLayout.myOrder
         myOrder.ivIcon.setImageResource(R.drawable.ic_myorder)
         myOrder.tvName.text = "My Order"
+
+        myOrder.root.setOnClickListener {
+            Intent(requireContext(), OrderActivity::class.java)
+                .also {
+                    startActivity(it)
+                }
+        }
 
         val myReturn = binding.authLayout.myReturn
         myReturn.ivIcon.setImageResource(R.drawable.ic_myreturn)
