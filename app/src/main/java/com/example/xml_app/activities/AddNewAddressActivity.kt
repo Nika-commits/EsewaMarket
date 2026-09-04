fe163d91176b1b24d853e1f943d24c4c15331e70
Your Name <itismepranish@gmail.com>
2026-09-04 12:21:24 +0545

    chore: Added Delete Bottom Sheet UI in Edit Activity

--- app/src/main/java/com/example/xml_app/activities/AddNewAddressActivity.kt
+++ app/src/main/java/com/example/xml_app/activities/AddNewAddressActivity.kt
@@ -19,23 +19,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,6 +68,7 @@ import com.example.xml_app.utils.styles.components.AppTextField
 import com.example.xml_app.utils.styles.components.AppTopBar
 import com.example.xml_app.utils.styles.components.ButtonVariant
 import com.example.xml_app.viewModel.AddNewAddressViewModel
+import kotlinx.coroutines.launch
 
 class AddNewAddressActivity : AppCompatActivity() {
     companion object {
@@ -108,6 +116,7 @@ class AddNewAddressActivity : AppCompatActivity() {
 
     private val viewModel: AddNewAddressViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val mode = when (intent.getStringExtra(TYPE)) {
@@ -123,6 +132,10 @@ class AddNewAddressActivity : AppCompatActivity() {
 
         setContent {
             val snackbarHostState = remember { SnackbarHostState() }
+            val sheetState = rememberModalBottomSheetState()
+            val scope = rememberCoroutineScope()
+            var showBottomSheet by remember { mutableStateOf(false) }
+
             LaunchedEffect(Unit) {
                 viewModel.snackbarMessage.collect { message ->
                     snackbarHostState.showSnackbar(message)
@@ -186,7 +199,7 @@ class AddNewAddressActivity : AppCompatActivity() {
                                 viewModel.saveAddress(mode, addressId)
                             },
                             onDelete = {
-
+                                showBottomSheet = true
                             },
                             isSaving = isSaving,
                             onEvent = {
@@ -199,10 +212,94 @@ class AddNewAddressActivity : AppCompatActivity() {
                         )
                     }
                 }
+
+                if (showBottomSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showBottomSheet = false
+                        },
+                        sheetState = sheetState,
+                        containerColor = Surface,
+                        dragHandle = null
+                    ) {
+                        DeleteBottomSheetContent(
+//                            modifier = Modifier.padding(innerPadding),
+                            onSheetClose = {
+                                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                    if (!sheetState.isVisible) {
+                                        showBottomSheet = false
+                                    }
+                                }
+                            },
+                            onDelete = {}
+                        )
+                    }
+
+                }
             }
         }
     }
+}
 
+@Composable
+fun DeleteBottomSheetContent(
+    modifier: Modifier = Modifier,
+    onSheetClose: () -> Unit,
+    onDelete: () -> Unit,
+    isDeleting: Boolean = false
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp, bottom = 16.dp, start = 16.dp, end = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Delete Address",
+                fontFamily = SourceSansPro,
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp,
+                letterSpacing = 0.15.sp,
+                textAlign = TextAlign.Center,
+                color = TextDark400
+            )
+            Text(
+                "Are you sure you want to delete address?",
+                fontFamily = SourceSansPro,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                letterSpacing = 0.25.sp,
+                textAlign = TextAlign.Center,
+                color = TextDark300,
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            AppButton(
+                modifier = Modifier.weight(1f),
+                variant = ButtonVariant.SECONDARY,
+                text = "CANCEL",
+                onClick = onSheetClose
+            )
+
+            AppButton(
+                modifier = Modifier.weight(1f),
+                variant = ButtonVariant.PRIMARY,
+                text = "DELETE",
+                onClick = onDelete,
+                isLoading = false
+            )
+        }
+    }
 }
 
 @Composable
@@ -458,27 +555,32 @@ fun DetailsFormScreen(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun FormPreview() {
-    DetailsFormScreen(
-        mode = AddNewAddressActivity.Companion.MODE.ADD,
-        address = CreateAddressRequest(
-            fullName = "Pranish Chaulagain",
-            phoneNumber = "9841890609",
-            fullAddress = "Gothatar-8, Kathmandu",
-            label = AddressLabel.Home,
-            isDefaultAddress = true,
-            isDefaultShippingAddress = true
-        ),
-        modifier = Modifier.fillMaxWidth(),
-        onSave = {
+//    DetailsFormScreen(
+//        mode = AddNewAddressActivity.Companion.MODE.ADD,
+//        address = CreateAddressRequest(
+//            fullName = "Pranish Chaulagain",
+//            phoneNumber = "9841890609",
+//            fullAddress = "Gothatar-8, Kathmandu",
+//            label = AddressLabel.Home,
+//            isDefaultAddress = true,
+//            isDefaultShippingAddress = true
+//        ),
+//        modifier = Modifier.fillMaxWidth(),
+//        onSave = {
+//
+//        },
+//        onEvent = {
+//
+//        },
+//        onOpenMaps = {
+//
+//        },
+//        onDelete = {},
+//        isSaving = false
+//    )
 
-        },
-        onEvent = {
-
-        },
-        onOpenMaps = {
-
-        },
-        onDelete = {},
-        isSaving = false
+    DeleteBottomSheetContent(
+        onSheetClose = {},
+        onDelete = {}
     )
 }
